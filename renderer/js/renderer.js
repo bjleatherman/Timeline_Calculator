a425a08f2dba2d577a29fd69111853e43c40f2da
bjleatherman <bjleatherman@gmail.com>
2025-01-06 22:19:58 -0500

    removed events comment that was a template

--- renderer/js/renderer.js
+++ renderer/js/renderer.js
@@ -49,65 +49,12 @@ var calendarEl = document.getElementById('calendar');
     selectable: true,
     dayMaxEvents: true, // allow "more" link when too many events
     contentHeight: 'auto',
-    // events: [
-    //   {
-    //     title: 'All Day Event',
-    //     start: '2024-06-01'
-    //   },
-    //   {
-    //     title: 'Long Event',
-    //     start: '2024-06-07',
-    //     end: '2024-06-10'
-    //   },
-    //   {
-    //     groupId: 999,
-    //     title: '0 Repeating Event',
-    //     start: '2024-06-09T16:00:00'
-    //   },
-    //   {
-    //     groupId: 999,
-    //     title: '1 Repeating Event',
-    //     start: '2024-06-16T16:00:00'
-    //   },
-    //   {
-    //     title: 'Conference',
-    //     start: '2024-06-11',
-    //     end: '2024-06-13'
-    //   },
-    //   {
-    //     title: 'Meeting',
-    //     start: '2024-06-12T10:30:00',
-    //     end: '2024-06-12T12:30:00'
-    //   },
-    //   {
-    //     title: 'Lunch',
-    //     start: '2024-06-12T12:00:00'
-    //   },
-    //   {
-    //     title: 'Meeting',
-    //     start: '2024-06-12T14:30:00'
-    //   },
-    //   {
-    //     title: 'Happy Hour',
-    //     start: '2024-06-12T17:30:00'
-    //   },
-    //   {
-    //     title: 'Dinner',
-    //     start: '2024-06-12T20:00:00'
-    //   },
-    //   {
-    //     title: 'Birthday Party',
-    //     start: '2024-06-13T07:00:00'
-    //   },
-    //   {
-    //     title: 'Click for Google',
-    //     url: 'http://google.com/',
-    //     start: '2024-06-28'
-    //   }
-    // ],
-    events: currEvents,
-    dateClick: function() {
-      console.log(`date clicked`);
+    // events: calEvents.events,
+    dateClick: function(info) {
+      handleDateClick(info);
+    }, 
+    eventClick: function(info) {
+      handleEventClick(info);
     }
   });
 
