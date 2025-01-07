dd545a2cb82cca644bb8e0ce0c22889d468094bd
bjleatherman <bjleatherman@gmail.com>
2025-01-06 22:31:48 -0500

    supported using utils.js isValid to check the date of the input dates

--- renderer/js/LocalModels/localBookModel.js
+++ renderer/js/LocalModels/localBookModel.js
@@ -32,7 +32,7 @@ class LocalBook {
             color,
         } = options;
 
-        if (!this.isValidDate(receiveDate) || !this.isValidDate(dueDate)) {
+        if (!isValidDate(receiveDate) || !isValidDate(dueDate)) {
             throw new Error("Invalid date format provided.");
         }
         this.groupId = null;
@@ -52,25 +52,6 @@ class LocalBook {
         // Placeholder for multiple users if necessary.
         this.userId = 1;
         this.userGroup = 1;
-    }
-
-    isValidDate(dateString) {
-        // Check if the string matches the format YYYY-MM-DD
-        const dateRegex = /^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])$/;
-        if (!dateRegex.test(dateString)) {
-            return false; // Fails format check
-        }
-    
-        // Split the string into components
-        const [year, month, day] = dateString.split('-').map(Number);
-    
-        // Check if the date components form a valid date
-        const date = new Date(year, month - 1, day); // Months are zero-indexed in JS Date
-        return (
-            date.getFullYear() === year &&
-            date.getMonth() === month - 1 &&
-            date.getDate() === day
-        );
     }
 }
 
